884aeabba028f6807c39cff4e1d53f7180edd571
wilsonwanfung <122155502+wilsonwanfung@users.noreply.github.com>
2023-12-30 15:30:48 +0800

    Update algotrade.py

--- algotrade.py
+++ algotrade.py
@@ -13,12 +13,20 @@ class AlgoEvent:
         self.wait_time = self.ma_len # in days
         self.arr_bbw = numpy.array([])
         self.bbw_len = 1*30 # length of bbw, only set to 30 as too high will yield no sequeeze, may change
-        self.arr_close_dict = {} # key to the corresponding arr_close
+        self.bbstd = 2
+        self.arr_close_dict = {} # instrument to the corresponding arr_close
+        self.allocationratio_per_trade = 0.3
+        self.risk_limit_portfolio = 0.2
+        self.cooldown = 15
+        self.openOrder = {}
+        self.netOrder = {}
 
 
     def start(self, mEvt):
         self.myinstrument = mEvt['subscribeList'][0]
+        self.no_instrument = len(mEvt['subscribeList'])
         self.evt = AlgoAPI_Backtest.AlgoEvtHandler(self, mEvt)
+        self.evt.update_portfolio_sl(sl=self.risk_limit_portfolio, resume_after=60*60*24*self.cooldown)
         self.evt.start()
 
 
@@ -26,33 +34,38 @@ class AlgoEvent:
         # set start time and arr_close(s) of all instruments in bd on the first call of this function
         if not self.start_time:
             self.start_time = bd[self.myinstrument]['timestamp']
-            for key in bd:
-                #self.evt.consoleLog(f"stock dict: {bd[key]}")
-                self.arr_close_dict[key] = numpy.array([])
+            for instrument in bd:
+                #self.evt.consoleLog(f"stock dict: {bd[instrument]}")
+                self.arr_close_dict[instrument] = numpy.array([])
+       
+
         
         # check if it is decision time
         if bd[self.myinstrument]['timestamp'] >= self.lasttradetime + timedelta(hours=24):
             # update arr_close(s)
             self.lasttradetime = bd[self.myinstrument]['timestamp']
             #self.evt.consoleLog(f"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
-            for key in bd:
-                lastprice = bd[key]['lastPrice']
-                arr_close = self.arr_close_dict[key]
+            for instrument in bd:
+                lastprice = bd[instrument]['lastPrice']
+                highprice = bd[instrument]['highPrice']
+                lowprice = bd[instrument]['lowPrice']
+                arr_close = self.arr_close_dict[instrument]
                 #self.evt.consoleLog(f"arr close: {arr_close}")
                 
                 
-                self.arr_close_dict[key] = numpy.append(self.arr_close_dict[key], lastprice)
-            
+                self.arr_close_dict[instrument] = numpy.append(self.arr_close_dict[instrument], lastprice)
+   
+                
                 # keep the most recent observations for arr_close (record of close prices)
-                self.arr_close_dict[key] = self.arr_close_dict[key][-self.ma_len::]
-            
+                self.arr_close_dict[instrument] = self.arr_close_dict[instrument][-self.ma_len::]
+              
             # check if we have waited the initial peroid
             if bd[self.myinstrument]['timestamp'] <= self.start_time + timedelta(days = self.wait_time):
                 return
             
             # execute the trading strat for all instruments
-            for key in bd:
-                self.execute_strat(bd, key)
+            for instrument in bd:
+                self.execute_strat(bd, instrument)
             
             
     def on_marketdatafeed(self, md, ab):
@@ -65,23 +78,24 @@ class AlgoEvent:
         pass
 
     def on_openPositionfeed(self, op, oo, uo):
-        pass
+        self.openOrder = oo
+        self.netOrder = op
     
     
     def find_sma(self, data, window_size):
         return data[-window_size::].sum()/window_size
 
-    # execute the trading strat for one instructment given the key and bd       
-    def execute_strat(self, bd, key):
+    # execute the trading strat for one instructment given the instrument and bd       
+    def execute_strat(self, bd, instrument):
         self.evt.consoleLog("---------------------------------")
         self.evt.consoleLog("Executing strat")
 
         # find sma, sd, 2 bbands, bbw, and lastprice
-        arr_close = self.arr_close_dict[key]
+        arr_close = self.arr_close_dict[instrument]
         sma = self.find_sma(arr_close, self.ma_len)
         sd = numpy.std(arr_close)
-        upper_bband = sma + 2*sd
-        lower_bband = sma - 2*sd
+        upper_bband = sma + self.bbstd * sd
+        lower_bband = sma - self.bbstd * sd
         bbw = (upper_bband-lower_bband)/sma
         lastprice = arr_close[-1]
         
@@ -92,7 +106,7 @@ class AlgoEvent:
         #is_sequeeze = self.is_sequeeze(self.arr_bbw)
         
         # debug
-        self.evt.consoleLog(f"name of instrument: { bd[key]['instrument'] }")
+        self.evt.consoleLog(f"name of instrument: { bd[instrument]['instrument'] }")
         #self.evt.consoleLog(f"datetime: {bd[self.myinstrument]['timestamp']}")
         #self.evt.consoleLog(f"sma: {sma}")
         #self.evt.consoleLog(f"upper: {upper_bband}")
@@ -102,20 +116,20 @@ class AlgoEvent:
         # check for sell signal (price crosses upper bband and rsi > 70)
         if lastprice >= upper_bband:
             # caclulate the rsi
-            rsi = self.find_rsi(arr_close, self.rsi_len)
-            self.evt.consoleLog(f"rsi: {rsi}")
+            rsi = talib.RSI(arr_close, timeperiod = self.rsi_len)
+            self.evt.consoleLog(f"rsi: {rsi[-1]}")
             # check for rsi
-            if rsi > 60:
+            if rsi[-1] > 70:
                 self.test_sendOrder(lastprice, -1, 'open', self.find_positionSize(lastprice, is_sequeeze))
                 self.evt.consoleLog(f"SELL SELL SELL SELL")
                 
         # check for buy signal (price crosses lower bband and rsi < 30)
         if lastprice <= lower_bband:
             # caclulate the rsi
-            rsi = self.find_rsi(arr_close, self.rsi_len)
+            rsi = talib.RSI(arr_close, timeperiod = self.rsi_len)
             self.evt.consoleLog(f"rsi: {rsi}")
             # check for rsi
-            if rsi < 30:
+            if rsi[-1] < 30:
                 self.test_sendOrder(lastprice, 1, "open", self.find_positionSize(lastprice, is_sequeeze))
                 self.evt.consoleLog(f"BUY BUY BUY BUY")
                 
@@ -128,22 +142,7 @@ class AlgoEvent:
         if len(arr_bbw) < self.bbw_len:
             return False
         return arr_bbw[-1] == arr_bbw.min()
-    
-    
-    def find_rsi(self, arr_close, window_size):
-        # we use previous day's close price as today's open price, which is not entirely accurate
-        deltas = numpy.diff(arr_close)
-        gains = deltas * (deltas > 0)
-        losses = -deltas * (deltas < 0)
-    
-        avg_gain = numpy.mean(gains[:window_size])
-        avg_loss = numpy.mean(losses[:window_size])
-    
-        rs = avg_gain / avg_loss
-        rsi = 100 - (100 / (1 + rs))
-    
-        return rsi
-        
+
         
     def test_sendOrder(self, lastprice, buysell, openclose, volume = 10):
         order = AlgoAPIUtil.OrderObject()
@@ -165,20 +164,22 @@ class AlgoEvent:
     def find_positionSize(self, lastprice, is_sequeeze):
         res = self.evt.getAccountBalance()
         availableBalance = res["availableBalance"]
-        ratio = 0.3
+        ratio = self.allocationratio_per_trade
         volume = (availableBalance*ratio) / lastprice
-        total =  availableBalance*ratio
-        while total < 0.3 * availableBalance:
+        total =  volume *  lastprice
+        while total < self.allocationratio_per_trade * availableBalance:
             ratio *= 1.05
             volume = (availableBalance*ratio) / lastprice
-            total = availableBalance*ratio
+            total =  volume *  lastprice
         while total > availableBalance:
             ratio *= 0.95
             volume = (availableBalance*ratio) / lastprice
-            total = availableBalance*ratio
-        if is_sequeeze:
-            volume *= 5
+            total =  volume *  lastprice
         return volume
+    
+
+
+
     
 
 
